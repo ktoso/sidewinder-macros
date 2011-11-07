3eae78cd5eaabbe5b94303495858e8cea53b1052
Konrad Malawski <konrad.malawski@project13.pl>
2011-11-07 01:55:38 +0100

    whoops

--- common.sh
+++ common.sh
@@ -1,5 +1,5 @@
 # focus on "window demanding attention"
 # tip: requires this action to be bound to a keyboard shortcut in KDE4
 function focus_attention {
-  xdotool ctrl+alt+a
+  xdotool key ctrl+alt+a
 }
